de08ae51393a32ee569bf79db5e7cc189d0d9774
Shivam Meena <meenashivam9650@gmail.com>
2023-09-23 19:58:30 +0530

    netlify example and fix #3

---
+++ examples/node/src/lib/session.ts
@@ -0,0 +1,18 @@
+import { RedisSessionStore, type nodeRedisSessionOptions } from "@ethercorps/sveltekit-redis-session";
+import { SECRET, REDIS_URL } from '$env/static/private';
+import {createClient} from "redis";
+
+const redisClient  = createClient( { url: REDIS_URL });
+await redisClient.connect();
+
+const sessionOptions: nodeRedisSessionOptions = {
+	redisClient: redisClient,
+	secret: SECRET,
+	sessionPrefix: 'netlify-node-srs-example-session',
+	userSessionsPrefix: 'netlify-node-srs-example-user',
+	cookieName: 'session',
+	cookiesOptions: {
+		maxAge: 10 * 60
+	}
+};
+export const sessionManager = new RedisSessionStore(sessionOptions);
